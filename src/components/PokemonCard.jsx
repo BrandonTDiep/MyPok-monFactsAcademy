57819415f969bde8cfe663a3e5981e00013bfba1
Brandon Diep <brandondiep2711@gmail.com>
2024-08-30 14:32:46 -0700

    adding small margins to card details, added comments, and organized code a bit

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -17,6 +17,7 @@ const PokemonCard = ({ pokemon }) => {
     return (
         <Card className="hover:shadow-lg my-8 bg-gray-100 hover:border-black transition-colors duration-500">
             <div className='flex flex-col md:flex-row'>
+                {/* Card Image */}
                 <div className="w-full md:w-1/2 lg:w-1/4 h-auto overflow-hidden rounded-l-lg">
                     <img
                         src={`${pokemon.images.small}`}
@@ -27,6 +28,7 @@ const PokemonCard = ({ pokemon }) => {
 
                 <div className="w-full md:w-2/3 flex flex-col flex justify-center md:pl-5 mt-4 md:mt-0"> 
                     <CardContent className="text-left p-0 pl-4">
+                        {/* Card Title */}
                         <CardTitle className="text-lg font-semibold text-lg flex items-center justify-between flex-wrap">
                             <div>
                                 {pokemon.name}
@@ -37,28 +39,53 @@ const PokemonCard = ({ pokemon }) => {
                             <Button variant="link" className='p-0 font-normal mr-4' onClick={() => setShowStats(!showStats)}>{showStats ? "Hide Prices" : "Show Prices"}
                             </Button>
                         </CardTitle>
+
+                        {/* Card Description */}
                         {showStats ? 
                         (<CardDescription className='mt-3'>
-                            <p className="text-sm font-normal mb-1"><span className="font-medium mr-1">Average Sell Price:</span> ${pokemon.cardmarket.prices.averageSellPrice}</p>
-                            <p className="text-sm font-normal mb-1"><span className="font-medium mr-1">Trend Price:</span> ${pokemon.cardmarket.prices.trendPrice}</p>
-                            <p className="text-sm font-normal mb-1"><span className="font-medium mr-1">Low Price:</span> ${pokemon.cardmarket.prices.lowPrice}</p>
-
+                            <p className="text-sm font-normal mb-1">
+                                <span className="font-medium mr-1">Average Sell Price:</span> 
+                                ${pokemon.cardmarket.prices.averageSellPrice}
+                            </p>
+                            <p className="text-sm font-normal mb-1">
+                                <span className="font-medium mr-1">Trend Price:</span> 
+                                ${pokemon.cardmarket.prices.trendPrice}
+                            </p>
+                            <p className="text-sm font-normal mb-1">
+                                <span className="font-medium mr-1">Low Price:</span> 
+                                ${pokemon.cardmarket.prices.lowPrice}
+                            </p>
                         </CardDescription>
                         )
                         : 
                         (<CardDescription className='mt-3'>
                             {pokemon.abilities && pokemon.abilities.map((ability, index) => (
                                 <div key={index} className='mb-3'>
-                                    <h3 className='text-xs lg:text-sm'><span className="font-medium">Ability: </span>{ability.name}</h3>
+                                    <h3 className='text-xs lg:text-sm'>
+                                        <span className="font-medium mr-1">Ability: </span>
+                                        {ability.name}
+                                    </h3>
                                     <p className='text-xs lg:text-sm'>{ability.text}</p>
                                 </div>
                             ))}
 
                             {pokemon.attacks && pokemon.attacks.map((attack, index) => (
                                 <div key={index} className='mb-3'>
-                                    <h3 className='text-xs lg:text-sm'><span className="font-medium">Attack: </span>{attack.name} {attack.damage}</h3>
-                                    <p className='text-xs lg:text-sm'><span className="font-medium">Cost: </span> {attack.cost.join(', ')}</p>
-                                    {attack.text && <p className='text-xs lg:text-sm'><span className="font-medium">Effect: </span> {attack.text}</p>}
+                                    <h3 className='text-xs lg:text-sm'>
+                                        <span className="font-medium">Attack: </span>
+                                        <span className='ml-1'>{attack.name}</span>
+                                        <span className='ml-1'>{attack.damage}</span>
+                                    </h3>
+
+                                    <p className='text-xs lg:text-sm'>
+                                        <span className="font-medium mr-1">Cost: </span> 
+                                        {attack.cost.join(', ')}
+                                    </p>
+
+                                    {attack.text && 
+                                    <p className='text-xs lg:text-sm'>
+                                        <span className="font-medium mr-1">Effect: </span> {attack.text}
+                                    </p>}
                                 </div>
                             ))}
                         </CardDescription>
